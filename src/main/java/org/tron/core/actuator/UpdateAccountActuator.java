349e2eb843d52602d938e36646a8cf0ac99167fe
Yin Shucheng <yinshucheng@xiaomi.com>
2018-05-07 08:56:36 +0800

    add unit test for update_account api

--- src/main/java/org/tron/core/actuator/UpdateAccountActuator.java
+++ src/main/java/org/tron/core/actuator/UpdateAccountActuator.java
@@ -4,12 +4,21 @@ import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.extern.slf4j.Slf4j;
+import org.tron.common.utils.StringUtil;
+import org.tron.core.Wallet;
 import org.tron.core.capsule.AccountCapsule;
 import org.tron.core.capsule.TransactionResultCapsule;
+import org.tron.core.db.AccountStore;
 import org.tron.core.db.Manager;
 import org.tron.core.exception.ContractExeException;
 import org.tron.core.exception.ContractValidateException;
+import org.tron.protos.Contract;
 import org.tron.protos.Contract.AccountUpdateContract;
+import org.tron.protos.Protocol;
+import org.tron.protos.Protocol.Transaction.Result.code;
+
+
+import java.util.Iterator;
 
 @Slf4j
 public class UpdateAccountActuator extends AbstractActuator {
@@ -30,16 +39,47 @@ public class UpdateAccountActuator extends AbstractActuator {
       account.setAccountName(accountUpdateContract.getAccountName().toByteArray());
       dbManager.getAccountStore().put(accountUpdateContract.getOwnerAddress().toByteArray(),
           account);
+      result.setStatus(fee, code.SUCESS);
       return true;
     } catch (InvalidProtocolBufferException e) {
       logger.debug(e.getMessage(), e);
+      result.setStatus(fee, code.FAILED);
       throw new ContractExeException(e.getMessage());
     }
   }
 
   @Override
   public boolean validate() throws ContractValidateException {
-    // todo validate freq.
+    try {
+      if (!contract.is(Contract.AccountUpdateContract.class)) {
+        throw new ContractValidateException(
+                "contract type error, expected type [AccountUpdateContract], real type[" + contract
+                        .getClass() + "]");
+      }
+
+      Contract.AccountUpdateContract contract = this.contract.unpack(Contract.AccountUpdateContract.class);
+      if (!Wallet.addressValid(contract.getOwnerAddress().toByteArray())) {
+        throw new ContractValidateException("Invalidate address");
+      }
+
+      String readableOwnerAddress = StringUtil.createReadableString(contract.getOwnerAddress());
+
+      if (!dbManager.getAccountStore().has(contract.getOwnerAddress().toByteArray())) {
+        throw new ContractValidateException(
+                "Account[" + readableOwnerAddress + "] not exists");
+      }
+
+      String readableAccountName = contract.getAccountName().toString();
+      if (readableAccountName == null || readableAccountName.isEmpty()) {
+        throw new ContractValidateException(
+                "Account Name is empty"
+        );
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      throw new ContractValidateException(ex.getMessage());
+    }
+
     return true;
   }
 
--- src/test/java/org/tron/core/actuator/UpdateAccountActuatorTest.java
+++ src/test/java/org/tron/core/actuator/UpdateAccountActuatorTest.java
@@ -4,17 +4,24 @@ import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.AfterClass;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.tron.common.utils.ByteArray;
 import org.tron.common.utils.FileUtil;
 import org.tron.core.Constant;
+import org.tron.core.Wallet;
+import org.tron.core.capsule.AccountCapsule;
+import org.tron.core.capsule.TransactionResultCapsule;
 import org.tron.core.config.DefaultConfig;
 import org.tron.core.config.args.Args;
 import org.tron.core.db.Manager;
+import org.tron.core.exception.ContractExeException;
+import org.tron.core.exception.ContractValidateException;
 import org.tron.protos.Contract;
+import org.tron.protos.Protocol;
+import org.tron.protos.Protocol.Transaction.Result.code;
 
 import java.io.File;
 
@@ -24,6 +31,16 @@ public class UpdateAccountActuatorTest {
     private static AnnotationConfigApplicationContext context;
     private static Manager dbManager;
     private static final String dbPath = "output_UpdateAccountTest";
+
+    private static final String ACCOUNT_NAME_EXIST = "account_exist";
+    private static final String ACCOUNT_NAME_UPDATED = "account_updated";
+    private static final String ACCOUNT_NAME_EMPTY = "";
+
+    private static final String OWNER_ADDRESS_EXIST =
+            Wallet.getAddressPreFixString() + "abd4b9367799eaa3197fecb144eb71de1e049abc";
+    private static final String OWNER_ADDRESS_NOT_EXIST =
+            Wallet.getAddressPreFixString() + "444449367799eaa3197fec1144eb71de1e049abc";
+
 
     static {
         Args.setParam(new String[] {"--output-directory", dbPath}, Constant.TEST_CONF);
@@ -35,11 +52,20 @@ public class UpdateAccountActuatorTest {
         dbManager = context.getBean(Manager.class);
 
         // create a account for test
+        CreateAccountActuator createAccountActuator =
+                new CreateAccountActuator(getCreateAccountContract(ACCOUNT_NAME_EXIST, OWNER_ADDRESS_EXIST), dbManager);
+        TransactionResultCapsule createAccountRet = new TransactionResultCapsule();
+        try {
+            createAccountActuator.execute(createAccountRet);
+        } catch (ContractExeException e) {
+            e.printStackTrace();
+        }
+
 
 
     }
 
-    private Any getContract(String name, String address) {
+    private static Any getUpdateAccountContract(String name, String address) {
         return Any.pack(
                 Contract.AccountUpdateContract.newBuilder()
                 .setAccountName(ByteString.copyFromUtf8(name))
@@ -48,18 +74,69 @@ public class UpdateAccountActuatorTest {
         );
     }
 
+
+    private static Any getCreateAccountContract(String name, String address) {
+        return Any.pack(
+                Contract.AccountCreateContract.newBuilder()
+                        .setAccountName(ByteString.copyFromUtf8(name))
+                        .setOwnerAddress(ByteString.copyFrom(ByteArray.fromHexString(address)))
+                        .build());
+    }
+
     @Test
     public void rightlUpdateAccount() {
+        // update account
+        UpdateAccountActuator actuator =
+                new UpdateAccountActuator(getUpdateAccountContract(ACCOUNT_NAME_UPDATED, OWNER_ADDRESS_EXIST), dbManager);
+        TransactionResultCapsule ret = new TransactionResultCapsule();
 
+        try {
+            actuator.validate();
+            actuator.execute(ret);
+
+            AccountCapsule owner = dbManager.getAccountStore().get(ByteArray.fromHexString(OWNER_ADDRESS_EXIST));
+            Assert.assertEquals(ACCOUNT_NAME_UPDATED, owner.getInstance().getAccountName().toStringUtf8());
+            Assert.assertEquals(code.SUCESS, ret.getInstance().getRet());
+
+        } catch (ContractExeException e) {
+            Assert.assertFalse(e instanceof ContractExeException);
+        } catch (ContractValidateException e) {
+            Assert.assertFalse(e instanceof ContractValidateException);
+        }
     }
 
     @Test
     public void addressNotExistUpdateAccount() {
+        UpdateAccountActuator actuator =
+                new UpdateAccountActuator(getUpdateAccountContract(ACCOUNT_NAME_UPDATED, OWNER_ADDRESS_NOT_EXIST), dbManager);
+        TransactionResultCapsule ret = new TransactionResultCapsule();
 
+        try {
+            actuator.validate();
+            actuator.execute(ret);
+        }  catch (ContractValidateException e) {
+            Assert.assertTrue(e instanceof ContractValidateException);
+            Assert.assertEquals(e.getMessage(), "Account[" + OWNER_ADDRESS_NOT_EXIST + "] not exists");
+        } catch (ContractExeException e) {
+            Assert.assertFalse(e instanceof ContractExeException);
+        }
     }
 
     @Test
     public void accountNameNullOrEmptyUpdateAccount() {
+        UpdateAccountActuator actuator =
+                new UpdateAccountActuator(getUpdateAccountContract(ACCOUNT_NAME_EMPTY, OWNER_ADDRESS_EXIST), dbManager);
+        TransactionResultCapsule ret = new TransactionResultCapsule();
+
+        try {
+            actuator.validate();
+            actuator.execute(ret);
+        }  catch (ContractValidateException e) {
+            Assert.assertTrue(e instanceof ContractValidateException);
+            Assert.assertEquals(e.getMessage(), "Account Name is empty");
+        } catch (ContractExeException e) {
+            Assert.assertTrue(e instanceof ContractExeException);
+        }
 
     }
 
